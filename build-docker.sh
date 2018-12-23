27399cf73ac1a0e1521fd9fcbc1e040c479662bb
aferrero2707 <aferrero1975@gmail.com>
2018-12-23 16:49:15 +0100

    Switched to MSYS2 build environment

--- build-docker.sh
+++ build-docker.sh
@@ -10,4 +10,7 @@ cp -a ci RawTherapee
 cd RawTherapee
 #docker run -it -v $(pwd):/sources -e "RT_BRANCH=$RT_BRANCH" photoflow/docker-centos7-gtk bash 
 #/sources/ci/appimage-centos7.sh
-docker run -it -e "TRAVIS_BUILD_DIR=/sources" -e "TRAVIS_BRANCH=${RT_BRANCH}" -e "TRAVIS_COMMIT=${TRAVIS_COMMIT}" -v $(pwd):/sources photoflow/docker-buildenv-mingw bash #-c /sources/ci/package-w64.sh
+
+#docker run -it -e "TRAVIS_BUILD_DIR=/sources" -e "TRAVIS_BRANCH=${RT_BRANCH}" -e "TRAVIS_COMMIT=${TRAVIS_COMMIT}" -v $(pwd):/sources photoflow/docker-buildenv-mingw bash #-c /sources/ci/package-w64.sh
+
+docker run -it -e "TRAVIS_BUILD_DIR=/sources" -e "TRAVIS_BRANCH=${RT_BRANCH}" -e "TRAVIS_COMMIT=${TRAVIS_COMMIT}" -v $(pwd):/sources photoflow/docker-buildenv-mingw-manjaro bash #-c /sources/ci/package-w64.sh
