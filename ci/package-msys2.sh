aa9670621e0fc37b1005c66f73725776e2bcc47c
aferrero2707 <aferrero1975@gmail.com>
2018-12-23 18:36:12 +0100

    Using sudo for zip archive creation

--- ci/package-msys2.sh
+++ ci/package-msys2.sh
@@ -280,7 +280,7 @@ sudo pacman --noconfirm -S zip || exit 1
 
 rm -f $TRAVIS_BUILD_DIR/${bundle_package}_${bundle_version}.zip
 cd $repackagedir/../
-zip -q -r $TRAVIS_BUILD_DIR/${bundle_package}_${bundle_version}.zip $bundle_package-$bundle_version
+sudo zip -q -r $TRAVIS_BUILD_DIR/${bundle_package}_${bundle_version}.zip $bundle_package-$bundle_version
 #transfer $TRAVIS_BUILD_DIR/$bundle_package-$bundle_version.zip
 
 exit 
