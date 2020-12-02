508eaafd1d341d3ec4377fa197eda45921d4cd57
aferrero2707 <aferrero1975@gmail.com>
2020-12-02 08:30:01 +0100

    Update build-msys2.sh

--- ci/build-msys2.sh
+++ ci/build-msys2.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-sudo pacman-key --refresh-keys || exit 1
+#sudo pacman-key --refresh-keys || exit 1
+sudo pacman -S archlinux-keyring || exit 1
+sudo pacman-key --populate archlinux || exit 1
 sudo pacman --noconfirm -Syu || exit 1
 sudo pacman --noconfirm -S wget || exit 1
 (sudo mkdir -p /work && sudo chmod a+w /work) || exit 1
